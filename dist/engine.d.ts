84c276c3407bb84206ba898c4e07b266ce61cfb7
Craig Glaser <craigglaser@MacBook-Pro.local>
2026-02-28 12:20:07 -0500

    Cleanup git: remove tracked node_modules and dist, add .gitignore

--- dist/engine.d.ts
+++
@@ -1,7 +0,0 @@
-import { type Player, type Result, type GameState } from './types.js';
-export declare class Engine {
-    static simulatePlateAppearance(player: Player): Result;
-    static processResult(state: GameState, result: Result): GameState;
-    private static advanceRunners;
-}
-//# sourceMappingURL=engine.d.ts.map
--- dist/shop.d.ts
+++
@@ -1,6 +0,0 @@
-import { type Player } from './types.js';
-export declare class Shop {
-    static generateFreeAgents(count: number): Player[];
-    static tradePlayer(lineup: Player[], indexToReplace: number, newPlayer: Player): Player[];
-}
-//# sourceMappingURL=shop.d.ts.map
